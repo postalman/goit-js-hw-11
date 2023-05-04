4b4a4cf9f71e79cdd19ae0b939030b928949c611
ChillyRide <sebastianmars78@gmail.com>
2023-05-04 14:47:55 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ import './css/styles.css';
 const input = document.querySelector('input[searchQuery]');
 const btn = document.querySelector('button[type="submit"');
 
+btn.addEventListener('submit', onSubmit);
+
 
